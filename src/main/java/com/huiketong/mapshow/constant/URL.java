0b7005b364d550494f3001ca6a7451fc291c3dcd
zuofei <348068347@qq.com>
2019-05-30 17:28:56 +0800

    后台修改了上传内容方式

--- src/main/java/com/huiketong/mapshow/constant/URL.java
+++ src/main/java/com/huiketong/mapshow/constant/URL.java
@@ -5,4 +5,5 @@ public class URL {
     public static final String CHINA = "echarts/china.html";
     public static final String JIANGXI = "echarts/jiangxi.html";
     public static final String TOOLTIPS = "echarts/tooltips.html";
+    public static final String IMAGE_URL = "http://image.jkt365.com/";
 }
